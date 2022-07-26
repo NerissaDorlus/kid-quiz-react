b0c23bcb83be4e9032480793ac1b28da504c73a0
Neri D <nerissadorlus@gmail.com>
2022-07-26 18:52:24 -0400

    added 8 quiz questions

---
+++ src/questions.js
@@ -0,0 +1,96 @@
+const questions = [
+    {
+        question: "Find the missing number.  5 6 7 _ 9",
+        wrongAnswers: [
+            "4",
+            "2",
+            "10"
+        ],
+        rightAnswer: [
+            "8"
+        ]
+    },
+
+    {
+        question: "What color is the sun 🌞 ?",
+        wrongAnswers: [
+            "Green",
+            "Blue",
+            "Red"
+        ],
+        rightAnswer: [
+            "Yellow"
+        ]
+    },
+
+    {
+        question: "What sound does a pig make?",
+        wrongAnswers: [
+            "Moo",
+            "Woof",
+            "Meow"
+        ],
+        rightAnswer: [
+            "Oink"
+        ]
+    },
+
+    {
+        question: "What is the hottest season?",
+        wrongAnswers: [
+            "Fall (Autum)",
+            "Spring",
+            "Winter"
+        ],
+        rightAnswer: [
+            "Summer"
+        ]
+    },
+
+    {
+        question: "Find the missing letter. r s t _ v w",
+        wrongAnswers: [
+            "d",
+            "k",
+            "m"
+        ],
+        rightAnswer: [
+            "u"
+        ]
+    },
+
+    {
+        question: "Help us choose a healthy snack.",
+        wrongAnswers: [
+            "Ice cream",
+            "Donuts",
+            "Chocolate"
+        ],
+        rightAnswer: [
+            "Apple"
+        ]
+    },
+
+    {
+        question: "What should you do before bed?",
+        wrongAnswers: [
+            "Scream",
+            "Cartwheels",
+            "Cry"
+        ],
+        rightAnswer: [
+            "Brush your teeth"
+        ]
+    },
+    {
+        question: "How do you spell 🐶",
+        wrongAnswers: [
+            "C A T",
+            "P I G",
+            "R A T"
+        ],
+        rightAnswer: [
+            "D O G"
+        ]
+    }
+]
